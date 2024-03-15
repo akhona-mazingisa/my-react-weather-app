6fa86f66376b7b2a46aee2fa5d1dc2e0ed41465a
akhona-mazingisa <akhonamazingisa04@gmail.com>
2024-03-15 15:20:32 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by {" "} <a href="https://github.com/akhona-mazingisa" target="_blank" rel="noreferrer">
+          Akhona Mazingisa
+          </a> {" "}
+          and is {" "}
+      <a href="https://github.com/akhona-mazingisa/my-react-weather-app" target="_blank" rel="noreferrer"> 
+      open-sourced on GitHub </a>
+      </footer>
       </div>
-  );
+      );
 }
 
 
