bfbe0dcb16f949080b0fca04d2eb45f568ea7f8b
akhona-mazingisa <akhonamazingisa04@gmail.com>
2024-03-18 14:15:39 +0200

    Created a new component and tracked the unit of that component so that different temperature can be displayed.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,11 +1,12 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (
         <div className="WeatherInfo">
-        <h1>{props.data.city}</h1>
+            <h1>{props.data.city}</h1>
                 <ul>
                     <li>
                         <FormattedDate date={props.data.date} />
@@ -17,21 +18,18 @@ export default function WeatherInfo(props) {
                         <div className="clearfix">
                             <div className="float-left">
                                 <WeatherIcon code={props.data.icon} />
+                                
+                                    <WeatherTemperature celsius={props.data.temperature} />
+                                </div>
                             </div>
-                            <div className="float-left">
-                                <span className="temperature">
-                                    {Math.round(props.data.temperature)}</span>
-                                <span className="Unit">°C</span>
-                            </div>
-                        </div>
                         </div>
                     <div className="col-6">
                         <ul>
                             <li>Humidity: {props.data.humidity}%</li>
                             <li>Wind: {props.data.wind} km/h</li>
                         </ul>
-                    </div>
                 </div>
             </div>
+        </div>
     );
 }
